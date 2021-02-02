e21714677344553ab1ffbf1744651dc0273533e9
Christian Bartz <christian.bartz@hpi.de>
2021-02-02 17:48:04 +0100

    make json dataset more reusable

--- pytorch_training/data/json_dataset.py
+++ pytorch_training/data/json_dataset.py
@@ -1,33 +1,36 @@
 import json
-import os
 from collections import Callable
+from pathlib import Path
+from typing import Dict, Union
 
-import numpy
+import torch
 from torch.utils import data
-from typing import Dict
 
-from pytorch_training.data.utils import default_loader, is_image_file
+from pytorch_training.data.utils import default_loader
 from pytorch_training.images import is_image
 
 
 class JSONDataset(data.Dataset):
 
-    def __init__(self, json_file: str, root: str = None, transforms: Callable = None, loader: Callable = default_loader):
-        with open(json_file) as f:
-            self.image_data = json.load(f)
-            self.image_data = [file_path for file_path in self.image_data if is_image(file_path)]
+    def __init__(self, json_file: Union[str, Path], root: Union[str, Path] = None, transforms: Callable = None, loader: Callable = default_loader):
+        with Path(json_file).open() as f:
+            self.load_json_data(json.load(f))
 
-        self.root = root
+        self.root = Path(root)
         self.transforms = transforms
         self.loader = loader
+
+    def load_json_data(self, json_data: Union[dict, list]):
+        self.image_data = json_data
+        self.image_data = [file_path for file_path in self.image_data if is_image(file_path)]
 
     def __len__(self):
         return len(self.image_data)
 
-    def __getitem__(self, index: int) -> Dict[str, numpy.ndarray]:
+    def __getitem__(self, index: int) -> Dict[str, torch.Tensor]:
         path = self.image_data[index]
         if self.root is not None:
-            path = os.path.join(self.root, path)
+            path = self.root / path
 
         image = self.loader(path)
 
